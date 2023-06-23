d255f4867fb0922720d2f5480ad39e8db1d7b272
Andrew Gleeson <heygleeson@gmail.com>
2023-06-23 17:24:26 +1000

    Added ToC, sticky nav menus.

--- eleventy/eleventy.config.js
+++ eleventy/eleventy.config.js
@@ -1,6 +1,12 @@
 // ELEVENTY CONFIG
 const { DateTime } = require("luxon");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginTOC = require('eleventy-plugin-toc');
+
+// markdown-it + plugins
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+
 
 module.exports = function(config) {
 
@@ -33,6 +39,11 @@ module.exports = function(config) {
 
 	// Plugin: Eleventy Navigation
 	config.addPlugin(pluginNavigation);
+
+	// Plugin: Table of Contence
+	config.addPlugin(pluginTOC, {
+		ul: true
+	});
 	
 	// DEV: Don't duplicate passthru folders into '/build/'
 	config.setServerPassthroughCopyBehavior("passthrough");
@@ -40,6 +51,9 @@ module.exports = function(config) {
 	// Passthrough Folders
 	config.addPassthroughCopy({"src/_": "."})
 	config.addPassthroughCopy("src/img")
+
+	// Set parser library
+	config.setLibrary('md', markdownIt().use(markdownItAnchor));
 
 	// Return config options
 	return {
