64e86197bb58bad1af57895fab10819275b1d52b
Andrew Gleeson <heygleeson@gmail.com>
2023-06-24 13:43:45 +1000

    Rewrote theme handling to avoid flicker.

--- eleventy/src/_/js/script.js
+++ eleventy/src/_/js/script.js
@@ -1,36 +1,29 @@
-console.log("HELLO");
-
 const buttonLight = document.getElementById('lightToggle');
 const lightIcon = document.getElementById('lighticon');
 const emblem = document.getElementById('emblem');
 
-// Get lightmode state from storage. Returns a string or null, which is annoying.
-let lightMode = localStorage.getItem('lightmode');
-
-if (lightMode == null) {
-	// There's nothing in storage, so set a default value.
-	localStorage.setItem('lightmode', false);
-	lightMode = false;
-} else {
-	// If the string matches 'true', then convert it to a boolean.
-	lightMode = lightMode == 'true';
-	if (lightMode) toggleLightMode(lightMode);
+if (theme == 'system') {
+	theme = (window.matchMedia('(prefers-color-scheme: light)').matches) ? 'light' : 'dark';
 }
 
-buttonLight.addEventListener('click', () => {
-	lightMode = !lightMode;
-	localStorage.setItem('lightmode', lightMode);
-	toggleLightMode(lightMode);
-});
+document.documentElement.dataset.theme = theme;
+setTheme(theme);
 
-function toggleLightMode(isLight) {
-	if (isLight) {
-		document.body.classList.add('lightmode');
+function setTheme(theme) {
+	document.documentElement.dataset.theme = theme;
+
+	// Update Icons/Emblems
+	if (theme == 'light') {
 		lightIcon.classList.add('fa-moon');
 		emblem.src = '/img/Emblem_Black_100px.png';
 	} else {
-		document.body.classList.remove('lightmode');
 		lightIcon.classList.remove('fa-moon');
 		emblem.src = '/img/Emblem_White_100px.png';
 	}
 }
+
+buttonLight.addEventListener('click', () => {
+	theme = (theme == 'light') ? 'dark' : 'light';
+	localStorage.setItem('theme', theme);
+	setTheme(theme);
+});
